d67aa833aeddc5018933bdecace5fcdc9a32ab4d
Alec Hon <alechon2010@gmail.com>
2018-11-13 17:40:27 -0800

    Edit Code - filter out too many missing values, filter out footnotes

--- Project_6_Code.R
+++ Project_6_Code.R
@@ -12,5 +12,17 @@ poverty <- data %>% filter(Indicator.Code == "SI.POV.LMIC.GP")
 pov12 <- poverty %>% filter(!X2012 %in% NA)
 wdi <- data %>% filter(data$Country.Name %in% pov12$Country.Name)
 
-trial <- wdi %>% gather(year,values,c(3:ncol(wdi)))
+wdi <- wdi %>% gather(year,values,c(3:ncol(wdi)))
+wdi <- wdi  %>% spread(Indicator.Code, values) 
 
+missing <- wdi %>% summarize_all(funs(sum(is.na(.))/n()))
+missing <- gather(missing, key="feature", value="missing_pct")
+goodv <- filter(missing, missing_pct<0.25)
+
+wdi_good <- wdi %>% select(goodv$feature)
+
+wdi_good %>% write_csv('wdi_good.csv')
+
+footnote <- data.frame(read_excel('W03b_wdi.xlsx', sheet = 'FootNote'))
+footnote1 <- footnote %>% subset(footnote$SeriesCode %in% goodv$feature)
+footnote1 %>% write_csv('footnote_good.csv')
